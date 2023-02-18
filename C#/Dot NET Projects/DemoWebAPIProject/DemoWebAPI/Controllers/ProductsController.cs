4824d7cfb9dc7da9c44fde1016eaf6678ea414e4
PranjalGaurUW <p5gaur@uwaterloo.ca>
2023-02-17 19:31:55 -0500

    changes in productcontroller

--- C#/Dot NET Projects/DemoWebAPIProject/DemoWebAPI/Controllers/ProductsController.cs
+++ C#/Dot NET Projects/DemoWebAPIProject/DemoWebAPI/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DemoWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,18 +21,62 @@ namespace DemoWebAPI.Controllers
             _context.Database.EnsureCreated();//Run seeding
         }
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task<IActionResult> GetProducts()
         {
-            return Ok(_context.Products.ToList());
+            return Ok(await _context.Products.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetProduct(int id)
+        public async Task<IActionResult> GetProduct(int id)
         {
-            var product = _context.Products.Find(id);//finds an entity with the given primary key value
+            var product = await _context.Products.FindAsync(id);//finds an entity with the given primary key value
             if(product==null)
                 return NotFound();
             else return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product
+                );
+        }
+
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest();
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(x => x.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var item = await _context.Products.FindAsync(id);
+            if (item == null) return NotFound();
+
+            _context.Products.Remove(item);
+            await _context.SaveChangesAsync();
+            return item;
         }
     }
 }
