b3f60659c1fefbe3c270f5cac351f4f4b60de560
PranjalGaurUW <p5gaur@uwaterloo.ca>
2023-05-10 23:11:11 -0400

    cleanup

--- Postgres(SQL) /Triggers/populate ledger trigger.sql
+++ Postgres(SQL) /Triggers/populate ledger trigger.sql
@@ -1,20 +1,10 @@
 
 
--- CREATE TRIGGER populate_ledger_trigger
--- AFTER UPDATE ON transactions
--- FOR EACH ROW
--- WHEN (OLD.value_in_usd IS DISTINCT FROM NEW.value_in_usd
---    OR OLD.quantity IS DISTINCT FROM NEW.quantity) 
--- EXECUTE PROCEDURE populate_ledger();
+CREATE TRIGGER populate_ledger_trigger
+AFTER UPDATE ON transactions
+FOR EACH ROW
+WHEN (OLD.value_in_usd IS DISTINCT FROM NEW.value_in_usd
+	OR OLD.quantity IS DISTINCT FROM NEW.quantity) 
 
 
-
-
-
-
-
-
-
-
-
-
+EXECUTE PROCEDURE populate_ledger();
--- React/ToDo List App/src/components/ToDoItem.jsx
+++ React/ToDo List App/src/components/ToDoItem.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ToDoItem(props)
 {
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClick(event)
+    {
+        setIsDone(prevValue => !prevValue);
+    }
     return (
-        <li>{props.text}</li>
+        <div onClick={handleClick}>
+            <li style={{textDecoration:isDone ? "line-through" : "none"}}>{props.text}</li>
+        </div>
+        
     );
 }
 export default ToDoItem;
